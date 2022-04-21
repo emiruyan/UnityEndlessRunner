90f589c4a3e17b56277d0de414c1543f02aa0928
emiruyan <emir-uyn@hotmail.com>
2022-04-21 22:02:32 +0300

    Enemy Scripts
    Enemy Spawn işlemi tamamlandı ve scriptleri giridli

---
+++ UnityEndlessRunnerProject/Assets/Game Folders/Scripts/Concretes/Controllers/SpawnerController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using Random = UnityEngine.Random;
+
+namespace  UnityEndlessRunnerProject.Controllers
+{
+    public class SpawnerController : MonoBehaviour
+    {
+        [SerializeField] EnemyController _enemyPrefab;
+        [Range(0.1f, 5f)][SerializeField] float _min = 0.1f;
+        [Range(6f, 15f)][SerializeField] float _max = 15f;
+        
+        float _maxSpawnTime;
+        float _currentSpawnTime = 0f;
+
+
+        private void OnEnable()
+        {
+            
+            GetRandomMaxTime();
+        }
+
+        private void Update()
+        {
+            _currentSpawnTime += Time.deltaTime;
+            
+            if (_currentSpawnTime > _maxSpawnTime)
+            {
+                 Spawn(); 
+            }
+            
+            
+        }
+
+        private void Spawn()
+        {
+            EnemyController newEnemy = Instantiate(_enemyPrefab, transform.position, transform.rotation);
+            newEnemy.transform.parent = this.transform;
+            
+            _currentSpawnTime = 0f;
+            GetRandomMaxTime();
+            
+        }
+
+        private void GetRandomMaxTime()
+        {
+            _maxSpawnTime = Random.Range(_min, _max);
+        }
+    }
+    
+    
+    
+    
+}
+
+
